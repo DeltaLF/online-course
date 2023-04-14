e563079924dc9c80f679fd862f024c21cdde2d31
Patrick <patricktsaikl@gmail.com>
2023-04-14 10:56:36 +0800

    fefactor: properly type redux store
    1. move redux store from index.js to a new file
    2. export state type

---
+++ src/redux/store.ts
@@ -0,0 +1,23 @@
+import { createStore, applyMiddleware, compose } from "redux";
+import reduxThunk from "redux-thunk";
+import reducers from "../reducers";
+import { messageClearMiddleware } from "../actions";
+
+declare global {
+  interface Window {
+    __REDUX_DEVTOOLS_EXTENSION_COMPOSE__?: typeof compose;
+  }
+}
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const store = createStore(
+  reducers,
+  composeEnhancers(applyMiddleware(reduxThunk, messageClearMiddleware))
+);
+
+export default store;
+// Infer the `RootState` and `AppDispatch` types from the store itself
+export type RootState = ReturnType<typeof store.getState>;
+// Inferred type: {posts: PostsState, comments: CommentsState, users: UsersState}
+export type AppDispatch = typeof store.dispatch;
